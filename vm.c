ae9a4f5aa15069887959fa8890b66150dcb89f69
youssefhj <yf.hajib@gmail.com>
2026-05-03 18:37:11 -0400

    ant Virtual Machine (VM)

---
+++ vm.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+#include "vm.h"
+#include "common.h"
+#include "debug.h"
+#include "compiler.h"
+
+VM vm;
+
+void initVM() {
+	initChunk(&vm.chunk);
+	vm.ip = NULL;
+	vm.topStack = vm.stack;
+}
+
+void freeVM() {
+	freeChunk(&vm.chunk);
+}
+
+static void push(Value value) {
+	vm.topStack++;
+	vm.topStack[-1] = value;
+}
+
+static Value pop() {
+	vm.topStack--;
+	return *vm.topStack;
+}
+
+static Value peek(int depth) {
+	return vm.topStack[-depth - 1];
+}
+
+static InterpretResult run() {
+	#define READ_BYTE()		(*vm.ip++)
+	#define READ_CONSTANT()		(vm.chunk.constants.values[READ_BYTE()])
+	
+	uint8_t instruction;
+	for (;;) {
+		#ifdef DEBUG_STACK_TRACE
+		printf("           ");
+		for (Value* slot = vm.stack; slot < vm.topStack; slot++) {
+			printf("[ ");
+			printValue(*slot);
+			printf(" ]");
+		}
+		printf("\n");
+		disassembleInstruction(&vm.chunk, (int)(vm.ip - vm.chunk.code));
+		#endif
+		switch (instruction = READ_BYTE()) {
+			case OP_CONSTANT:
+				push(READ_CONSTANT());
+				break;
+			case OP_PRINT:
+				printValue(pop());
+				break;
+			case OP_RETURN:
+				return INTERPRET_SUCCESS;
+
+		}
+		
+	}
+	
+	#undef READ_BYTE
+	#undef READ_CONSTANT
+
+}
+
+InterpretResult interpret(const char* source) {
+	initVM();
+
+	if (!compile(source, &vm.chunk)) {
+		return INTERPRET_COMPILETIME_ERROR;
+	}
+
+	vm.ip = vm.chunk.code;
+	InterpretResult result = run();
+	
+	freeVM();
+	return result;
+}
---
+++ vm.h
@@ -0,0 +1,31 @@
+#ifndef ANT_VM_H
+#define ANT_VM_H
+
+#include "common.h"
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX_SIZE 1024
+
+
+typedef enum {
+	INTERPRET_COMPILETIME_ERROR,
+	INTERPRET_RUNTIME_ERROR,
+	INTERPRET_SUCCESS
+} InterpretResult;
+
+
+typedef struct {
+	Chunk chunk;
+	uint8_t* ip;
+	Value stack[STACK_MAX_SIZE];
+	Value* topStack;
+} VM;
+
+
+void initVM();
+void freeVM();
+InterpretResult interpret(const char* source);
+
+
+#endif // ANT_VM_H
