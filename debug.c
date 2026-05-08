46e2a3b716b7e1746e4a451baf9c2e370ff7cb95
youssefhj <yf.hajib@gmail.com>
2026-05-08 11:48:08 -0400

    disassemble more opcodes: add, subtract, multiply and divide

--- debug.c
+++ debug.c
@@ -28,6 +28,14 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 
 	uint8_t instruction = chunk->code[offset];
 	switch (instruction) {
+		case OP_ADD:
+			return simpleInstruction("OP_ADD", offset);
+		case OP_SUBTRACT:
+			return simpleInstruction("OP_SUBTRACT", offset);
+		case OP_MULTIPLY:
+			return simpleInstruction("OP_MULTIPLY", offset);
+		case OP_DIVIDE:
+			return simpleInstruction("OP_DIVIDE", offset);
 		case OP_CONSTANT:
 			return constantInstruction("OP_CONSTANT", chunk, offset);
 		case OP_PRINT:
