4cc2ab345e9954fa38b36e439f63d785fbc8fab5
youssefhj <yf.hajib@gmail.com>
2026-05-09 20:03:45 -0400

    update the Value structure to support numbers, booleans and nil values

--- value.h
+++ value.h
@@ -1,8 +1,34 @@
 #ifndef ANT_VALUE_H
 #define ANT_VALUE_H
 
+#include "common.h"
 
-typedef double Value;
+#define NUMBER_VAL(value)          ((Value){VAL_NUMBER, {.number = value}})
+#define BOOL_VAL(value)            ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL                    ((Value){VAL_NIL, {.number = 0}})
+
+#define AS_NUMBER(value)           ((value).as.number)
+#define AS_BOOL(value)             ((value).as.boolean)
+#define AS_NIL(value)              ((value).as.number)
+
+#define IS_NUMBER(value)           ((value).type == VAL_NUMBER)
+#define IS_BOOL(value)             ((value).type == VAL_BOOL)
+#define IS_NIL(value)              ((value).type == VAL_NIL)
+
+typedef enum {
+	VAL_NUMBER,
+	VAL_BOOL,
+	VAL_NIL
+} ValueType;
+
+typedef struct {
+	ValueType type;
+
+	union {
+		double number;
+		bool boolean;
+	} as;
+} Value;
 
 typedef struct {
 	int capacity;
