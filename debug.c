1c74c4ca7ec5e94adf902a77302c31f9fb97f4c3
youssefhj <yf.hajib@gmail.com>
2026-05-11 11:06:26 -0400

    disassemble more opcodes: true, false and nil

--- debug.c
+++ debug.c
@@ -36,6 +36,12 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 			return simpleInstruction("OP_MULTIPLY", offset);
 		case OP_DIVIDE:
 			return simpleInstruction("OP_DIVIDE", offset);
+		case OP_TRUE:
+			return simpleInstruction("OP_TRUE", offset);
+		case OP_FALSE:
+			return simpleInstruction("OP_FALSE", offset);
+		case OP_NIL:
+			return simpleInstruction("OP_NIL", offset);
 		case OP_CONSTANT:
 			return constantInstruction("OP_CONSTANT", chunk, offset);
 		case OP_PRINT:
