fed6888f29f3576cc58831b4877ef90dd8e8c688
youssefhj <yf.hajib@gmail.com>
2026-05-11 18:25:30 -0400

    disassemble opcodes: 'equal' and 'not'

--- debug.c
+++ debug.c
@@ -42,6 +42,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 			return simpleInstruction("OP_FALSE", offset);
 		case OP_NIL:
 			return simpleInstruction("OP_NIL", offset);
+		case OP_EQUAL:
+			return simpleInstruction("OP_EQUAL", offset);
+		case OP_NOT:
+			return simpleInstruction("OP_NOT", offset);
 		case OP_CONSTANT:
 			return constantInstruction("OP_CONSTANT", chunk, offset);
 		case OP_PRINT:
