38f17521671b00d2d84b8ec50b03c05e68d6084f
youssefhj <yf.hajib@gmail.com>
2026-05-14 11:55:37 -0400

    addig new value type 'Obj'

--- value.h
+++ value.h
@@ -3,22 +3,28 @@
 
 #include "common.h"
 
+typedef struct Obj Obj;
+
 #define NUMBER_VAL(value)          ((Value){VAL_NUMBER, {.number = value}})
 #define BOOL_VAL(value)            ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL                    ((Value){VAL_NIL, {.number = 0}})
+#define OBJ_VAL(object)            ((Value){VAL_OBJ, {.obj = (Obj*)object}})     
 
 #define AS_NUMBER(value)           ((value).as.number)
 #define AS_BOOL(value)             ((value).as.boolean)
 #define AS_NIL(value)              ((value).as.number)
+#define AS_OBJ(value)              ((value).as.obj)
 
 #define IS_NUMBER(value)           ((value).type == VAL_NUMBER)
 #define IS_BOOL(value)             ((value).type == VAL_BOOL)
 #define IS_NIL(value)              ((value).type == VAL_NIL)
+#define IS_OBJ(value)              ((value).type == VAL_OBJ)
 
 typedef enum {
 	VAL_NUMBER,
 	VAL_BOOL,
-	VAL_NIL
+	VAL_NIL,
+	VAL_OBJ
 } ValueType;
 
 typedef struct {
@@ -27,6 +33,7 @@ typedef struct {
 	union {
 		double number;
 		bool boolean;
+		Obj* obj;
 	} as;
 } Value;
 
