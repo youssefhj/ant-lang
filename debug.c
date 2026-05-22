fdcf673b84f0d51309160ead8025ae2b5ce1cade
youssefhj <yf.hajib@gmail.com>
2026-05-22 10:19:36 -0400

    disassemble opcode 'pop'

--- debug.c
+++ debug.c
@@ -58,6 +58,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 			return simpleInstruction("OP_GREATER", offset);
 		case OP_NEGATE:
 			return simpleInstruction("OP_NEGATE", offset);
+		case OP_POP:
+			return simpleInstruction("OP_POP", offset);
 		case OP_DEFINE_GLOBAL:
 			return constantInstruction("OP_DEFINE_GLOBAL", chunk, offset);
 		case OP_GET_GLOBAL:
