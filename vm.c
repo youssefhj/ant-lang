edf61c44f0a180fb8b7e566d24e1637955c445a0
youssefhj <yf.hajib@gmail.com>
2026-06-08 11:32:16 -0400

    interpret bytecode: 'loop'

--- vm.c
+++ vm.c
@@ -197,6 +197,11 @@ static InterpretResult run() {
 				vm.ip += offset;
 				break;
 			}
+			case OP_LOOP: {
+				uint16_t offset = READ_SHORT();
+				vm.ip -= offset;
+				break;
+			}
 			case OP_CONSTANT:
 				push(READ_CONSTANT());
 				break;
