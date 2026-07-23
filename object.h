8fa37a09e363b4d80a15f4f6bb5766df59ca58a5
youssefhj <yf.hajib@gmail.com>
2026-07-23 12:38:00 -0400

    adding closure object and upvalue object

--- object.h
+++ object.h
@@ -10,13 +10,18 @@
 #define AS_STRING(value)          ((ObjString*)AS_OBJ(value))
 #define AS_CSTRING(value)         (((ObjString*)AS_OBJ(value))->chars)
 #define AS_FUNCTION(value)        ((ObjFunction*)AS_OBJ(value))
+#define AS_CLOSURE(value)         ((ObjClosure*)AS_OBJ(value))
 
 #define IS_STRING(value)          (isObjType(value, OBJ_STRING))
 #define IS_FUNCTION(value)        (isObjType(value, OBJ_FUNCTION))
+#define IS_CLOSURE(value)         (isObjType(value, OBJ_CLOSURE))
 
 typedef enum {
 	OBJ_STRING,
-	OBJ_FUNCTION
+	OBJ_FUNCTION,
+	OBJ_CLOSURE,
+	OBJ_UPVALUE,
+	OBJ_NATIVE
 } ObjType;
 
 struct Obj {
@@ -36,11 +41,28 @@ typedef struct {
 	ObjString* name;
 	int arity;
 	Chunk chunk;
+	int upvalueCount;
 } ObjFunction;
+
+typedef struct ObjUpvalue {
+	Obj obj;
+	Value* location;
+	Value closed;
+	struct ObjUpvalue* next;
+} ObjUpvalue;
+
+typedef struct {
+	Obj obj;
+	ObjFunction* function;
+	ObjUpvalue** upvalues;
+	int upvalueCount;
+} ObjClosure;
 
 ObjString* takeString(char* chars, int length);
 ObjString* copyString(const char* start, int length);
 ObjFunction* newFunction();
+ObjClosure* newClosure(ObjFunction* function);
+ObjUpvalue* newUpvalue(Value* slot);
 void printObject(Value value);
 
 static inline bool isObjType(Value value, ObjType type) {
