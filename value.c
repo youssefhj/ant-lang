574b6979034c006718a3ef448284ab9987e94968
youssefhj <yf.hajib@gmail.com>
2026-08-06 00:42:57 -0400

    NaN Boxing optimization

--- value.c
+++ value.c
@@ -22,15 +22,34 @@ int writeValueArray(ValueArray* array, Value value) {
 }
 
 void printValue(Value value) {
+#ifdef NAN_BOXING
+	if (IS_NUMBER(value)) {
+		printf("%g", AS_NUMBER(value));
+	} else if (IS_BOOL(value)) {
+		printf(AS_BOOL(value) ? "true" : "false");
+	} else if (IS_NIL(value)) {
+		printf("nil"); 
+	} else if (IS_OBJ(value)) {
+		printObject(value);
+	}
+#else
 	switch (value.type) {
 		case VAL_NUMBER: printf("%g", AS_NUMBER(value)); break;
 		case VAL_BOOL: printf(AS_BOOL(value) ? "true" : "false"); break;
 		case VAL_NIL: printf("nil"); break;
 		case VAL_OBJ: printObject(value); break;
 	}
+#endif
 }
 
 bool valuesEqual(Value a, Value b) {
+#ifdef NAN_BOXING
+	if (IS_NUMBER(a) && IS_NUMBER(b)) {
+		return AS_NUMBER(a) == AS_NUMBER(b);
+	}
+
+	return a == b;
+#else
 	if (a.type != b.type) return false;
 
 	switch (a.type) {
@@ -40,6 +59,7 @@ bool valuesEqual(Value a, Value b) {
 		default:
 		        return false;
 	}
+#endif
 }
 
 void freeValueArray(ValueArray* array) {
