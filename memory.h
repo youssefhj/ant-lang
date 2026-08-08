0b4ef5cc48215c1bb4b56ab1885cedd0cc90347d
youssefhj <yf.hajib@gmail.com>
2026-08-07 22:56:52 -0400

    changing 'reallocate' signature to accept the old allocation size so we can track the total bytes allocated and adding 'GC_HEAP_GROW_FACTOR' macro that grow the next GC threshold

--- memory.h
+++ memory.h
@@ -3,13 +3,18 @@
 
 #include "common.h"
 
-#define ALLOCATE(type, size)            ((type*)reallocate(NULL, sizeof(type)*(size)))
-#define GROW_CAPACITY(oldCapacity)      ((oldCapacity) < 8 ? 8 : ((oldCapacity) * 2))
-#define GROW_ARRAY(type, ptr, newSize)  ((type*)reallocate(ptr, sizeof(type)*(newSize)))
-#define FREE_ARRAY(type, ptr)           ((type*)reallocate(ptr, 0))
-#define FREE(type, ptr)                 ((type*)reallocate(ptr, 0))
+#define ALLOCATE(type, size)                           ((type*)reallocate(NULL, 0, sizeof(type)*(size)))
+#define GROW_CAPACITY(oldCapacity)                     ((oldCapacity) < 8 ? 8 : ((oldCapacity) * 2))
+#define GROW_ARRAY(type, ptr, oldSize, newSize)        ((type*)reallocate(ptr, sizeof(type)*(oldSize), sizeof(type)*(newSize)))
+#define FREE_ARRAY(type, ptr, oldSize)                 ((type*)reallocate(ptr, sizeof(type)*(oldSize), 0))
+#define FREE(type, ptr)                                ((type*)reallocate(ptr, sizeof(type), 0))
 
-void* reallocate(void* ptr, size_t newSize);
+#define GC_HEAP_GROW_FACTOR 2
+
+void* reallocate(void* ptr, size_t oldSize, size_t newSize);
+void markObject(Obj* object);
+void markValue(Value value);
+void collectGarbage();
 void freeObjects();
 
 #endif // ANT_MEMORY_H
